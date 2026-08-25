e90a9c57e8a1a2faa426e1858e92ab787e1ad4ad
luizfelipeosz <luizfelipeolsouz@gmail.com>
2026-08-25 10:31:57 -0300

    desenvolvimento das propriedades dos produtos para nas próximas atualizações já ter classes definidas.

---
+++ src/types/product.js
@@ -0,0 +1,13 @@
+/**
+ * @typedef {Object} Product
+ * @property {string} id
+ * @property {string} name
+ * @property {string} description
+ * @property {number} price
+ * @property {string} category
+ * @property {string} image
+ * @property {string[]} [images]
+ * @property {string} [brand]
+ * @property {boolean} [featured]
+ * @property {boolean} [isNew]
+ */
